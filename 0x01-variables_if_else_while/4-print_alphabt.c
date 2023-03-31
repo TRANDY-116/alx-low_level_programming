1356388af614ff8d5a35d28e3fb260d8ff157cdd
Tanwie Randy Funwie <randyfun16@gmail.com>
2023-03-31 16:48:09 +0100

    Update 4-print_alphabt.c
    using the && logical operator for "AND" also using "!=" the not equal to coupled with for loop

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,23 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 /**
- * main - Prints the alphabet without q and e.
- *
- * Return: Always 0 (Success)
+ * main - print all letters of the alphabet in lowercase
+ * except the letters q and e
+ * return 0 (success)
  */
 int main(void)
 {
-	int i;
-
-	for (i = 97; i < 123; i++)
-	{
-		if (i != 101 && i != 113)
-		{
-			putchar(i);
-		}
-	}
-	putchar('\n');
-	return (0);
+        int ch;
+        for (ch = 'a';ch <= 'z';ch++)
+        {
+                if (ch != 'q' && ch != 'e')
+                        putchar(ch);
+        }
+        putchar('\n');
+        return (0);
 }
